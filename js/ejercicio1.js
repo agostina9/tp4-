5294996cefcbb242d85815d3c338103b38099bcf
Agostina <lozagostina@gmail.com>
2022-07-24 23:23:54 -0300

    agrego ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,13 @@
+let auto={
+    color:negro,
+    marca:Audi,
+    modelo:A4,
+    //metodos
+    encender:function(){
+        document.write('<p>El auto se encendio</p>')
+    },
+    apagar:function(){
+        document.write('<p>El auto se apago</p>')
+    },
+
+}

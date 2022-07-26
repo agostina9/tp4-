c25f10fd8b43cb2b312e26ecd84a437fc1242785
Agostina <lozagostina@gmail.com>
2022-07-26 19:39:33 -0300

    agrego ejercicio 2 hecho en clases

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,37 +1,73 @@
 //con notacion literal
-let cuenta = {
-    titular:'Alex',
-    saldo:0,
-    transferir(){
-        console.log('stamos en el metodo transferir');
-        console.log(this);
-    },
+// let cuenta = {
+//     titular:'Alex',
+//     saldo:0,
+//     transferir(){
+//         console.log('stamos en el metodo transferir');
+//         console.log(this);
+//     },
+//     ingresar(deposito){
+//         console.log(this);
+//         this.saldo+=deposito;
+//          console.log('Se ingreso $'+ deposito);
+//         },
+
+//     extraer(retiro){
+//         if (this.saldo>=retiro){
+//             this.saldo -=retiro;
+//             document.write(`<p>Se retiro $${retiro}</p>`);
+//         }else{
+//             document.write(`<p>Saldo insuficiente</p>`);
+            
+//         }
+//     },
+
+//     informar(){
+//         document.write(`<p> La cuenta de ${this.titular}, posee un saldo de $${this.saldo}</p>`)
+//     },
+// }
+// cuenta.informar(cuenta);
+// console.log(this)
+
+// let deposito=parseFloat(prompt('Ingrese el monto deseado'));
+// cuenta.ingresar(deposito);
+
+// let retiro=parseFloat(prompt('Ingrese el monto a extraer'));
+// cuenta.extraer(retiro);
+// cuenta.informar();
+
+//con clases
+
+class Usuario{
+    constructor(titular, saldo){
+        this.titular=titular;
+        this.saldo=saldo;
+    }
     ingresar(deposito){
         console.log(this);
-        this.saldo+=deposito;
-         console.log('Se ingreso $'+ deposito);
-        },
-
+        this.saldo += deposito;
+        console.log('Se ingreso $'+ deposito)
+    }
     extraer(retiro){
-        if (this.saldo>=retiro){
-            this.saldo -=retiro;
-            document.write(`<p>Se retiro $${retiro}</p>`);
+        if(this.saldo>=retiro){
+            this.saldo -= retiro;
+            document.write(`<p>Se retiro $${retiro}</p>`)
         }else{
-            document.write(`<p>Saldo insuficiente</p>`);
-            
+            document.write(`<p>Saldo Insuficiente</p>`)
         }
-    },
-
+    }
     informar(){
         document.write(`<p> La cuenta de ${this.titular}, posee un saldo de $${this.saldo}</p>`)
-    },
+    }
 }
-cuenta.informar(cuenta);
-console.log(this)
+
 
 let deposito=parseFloat(prompt('Ingrese el monto deseado'));
-cuenta.ingresar(deposito);
 
 let retiro=parseFloat(prompt('Ingrese el monto a extraer'));
-cuenta.extraer(retiro);
-cuenta.informar();
+
+let Alex= new Usuario('Alex', '0')
+console.log(Alex)
+Alex.ingresar(deposito);
+Alex.extraer(retiro);
+Alex.informar();
